692abb666659753b0304f521c3fac97c4594d1d5
cavila <cristobal.avila@avventa.co.cr>
2013-02-19 18:51:57 -0600

    first activity

--- gen/com/cavila/kidsworldanimals/R.java
+++ gen/com/cavila/kidsworldanimals/R.java
@@ -14,7 +14,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f070007;
+        public static final int radioButton1=0x7f070002;
+        public static final int radioButton2=0x7f070003;
+        public static final int radioButton3=0x7f070004;
+        public static final int radioButton4=0x7f070005;
+        public static final int radioButton5=0x7f070006;
+        public static final int radioGroup=0x7f070001;
+        public static final int textView1=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
