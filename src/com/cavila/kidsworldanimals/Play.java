7dc8b8153eb098de831d7a93c3ad7976f4d57791
cavila <cristobal.avila@avventa.co.cr>
2013-02-20 18:49:04 -0600

    adding text to speech

--- src/com/cavila/kidsworldanimals/Play.java
+++ src/com/cavila/kidsworldanimals/Play.java
@@ -1,6 +1,7 @@
 package com.cavila.kidsworldanimals;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,7 +17,10 @@ public class Play extends Activity{
 	public void spellAnimal(View view){
 		//Toast.makeText(this, "Spelling...", Toast.LENGTH_SHORT).show();
 		SpeechTools spTools = new SpeechTools();
-		spTools.speechTest();
+		//spTools.speechTest();
+		spTools.speakOut("test");
+		
+		//startActivity(new Intent(Play.this, Play.class));
 		//myTTS.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
 	}
 }
--- src/com/cavila/kidsworldanimals/SpeechTools.java
+++ src/com/cavila/kidsworldanimals/SpeechTools.java
@@ -1,49 +1,77 @@
 package com.cavila.kidsworldanimals;
 
+import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
 import android.widget.Toast;
 import android.content.Context;
 import android.content.Intent;
 import java.util.Locale;
 
-public class SpeechTools /*implements OnInitListener*/ {
-	private int MY_DATA_CHECK_CODE = 0;
-	private TextToSpeech myTTS;
-	private Context context; //???
+public class SpeechTools implements OnInitListener {
+
+	 
+	
+	public void onInit(int status) {
+        // TODO Auto-generated method stub
+          //TTS is successfully initialized
+        if (status == TextToSpeech.SUCCESS) {
+                       //Setting speech language
+            int result = tts.setLanguage(Locale.US);
+           //If your device doesn't support language you set above
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                           //Cook simple toast message with message
+                //Toast.makeText(this, "Language not supported", Toast.LENGTH_LONG).show();
+                Log.e("TTS", "Language is not supported");
+            }
+                 //Enable the button - It was disabled in main.xml (Go back and Check it)
+                        else {
+               // btnSpeak.setEnabled(true);
+            }
+            //TTS is not initialized properly
+        } else {
+            //Toast.makeText(this, "TTS Initilization Failed", Toast.LENGTH_LONG).show();
+            Log.e("TTS", "Initilization Failed");
+        }
+    }
+	
+	public void speakOut(String text) {
+		
+		TextToSpeech tts;
+		//Initialize the tts object
+		tts = new TextToSpeech(context, listener)
+		//Get the text typed
+       //String text = txtText.getText().toString();
+        //If no text is typed, tts will read out 'You haven't typed text'
+        //else it reads out the text you typed
+       if (text.length() == 0) {
+           tts.speak("You haven't typed text", TextToSpeech.QUEUE_FLUSH, null);
+       } else {
+           tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+       }
+
+   }
+	
+	
+	public void onDestroy() {
+        // Don't forget to shutdown!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        this.onDestroy();
+    }	
+	
 	
 	public void speechTest(){
-		Toast.makeText(context, "Spelling...", Toast.LENGTH_SHORT).show();
-	}
-/*	
-	public void speakString(String word){
-		Intent checkTTSIntent = new Intent();
-		checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
-		startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);	
+		//Toast.makeText(this, "Spelling...", Toast.LENGTH_SHORT).show();
+		Log.e("sp class", "speechTest");
 	}
 	
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    if (requestCode == MY_DATA_CHECK_CODE) {
-	        if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
-	            myTTS = new TextToSpeech(this, this);
-	        }
-	        else {
-	            Intent installTTSIntent = new Intent();
-	            installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-	            startActivity(installTTSIntent);
-	        }
-	        }
-	}
+
 	
-	public void onInit(int initStatus) {
-	    if (initStatus == TextToSpeech.SUCCESS) {
-	        myTTS.setLanguage(Locale.US);
-	    }else if (initStatus == TextToSpeech.ERROR) {
-	        Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
-	    }
-	}
-	
-	*/
 	
 	
 }
